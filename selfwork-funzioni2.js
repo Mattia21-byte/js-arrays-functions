b4b1b53b3e2e393f7d3d9f1cb9b81d363ec5b62b
Mattia21-byte <mattiago2105@gmail.com>
2026-01-21 13:08:18 +0100

    esercizio FizzBuzz

---
+++ selfwork-funzioni2.js
@@ -0,0 +1,23 @@
+
+function fizzBuzz(N){
+
+    for(let i = 1; i <= N; i++){
+        if(i % 15 === 0){
+            console.log(`FizzBuzz`);
+            
+        }else if(i % 5 === 0){
+            console.log(`Buzz`);
+            
+        }else if(i % 3 === 0){
+            console.log(`Fizz`);
+            
+        }else{
+            console.log(i);
+            
+        }
+        
+    }
+
+}
+fizzBuzz(30);
+
