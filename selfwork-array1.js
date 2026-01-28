ab32edc26d2c5272a47dde5fef0a45a4c4603801
Mattia21-byte <mattiago2105@gmail.com>
2026-01-28 12:27:26 +0100

    esercizio js array, ordine numerico

---
+++ selfwork-array1.js
@@ -0,0 +1,11 @@
+
+let arr = [73, 8, -9, 9, 0, -44, 1000, 2, 21, 51];
+ 
+//ordine crescente
+arr.sort((a, b)=> a - b);
+console.log(arr);
+
+//ordine decrescente
+arr.sort((a, b)=> b -a);
+console.log(arr);
+
